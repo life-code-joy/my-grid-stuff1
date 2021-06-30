037fac410110eb18d52ae69dab0b8f795046c963
darren <darren__c@hotmail.com>
2021-06-30 11:59:24 +0900

    added sass and mixin.scss output a little js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const woodPieces = (woodpieces) => {
 const woodMachine = (wood) => {
   const totalWood = woodPieces(wood);
 
-  return `The total pieces of wood are ${totalWood}`;
+  return `The total pieces of wood are ${totalWood}. That is insane actually!!!`;
 };
 
 console.log(woodMachine(100));
@@ -13,8 +13,12 @@ console.log(woodMachine(100));
 //! change the css style
 const codeCSS = document.querySelector('.changeMe');
 const head3 = document.querySelector('.head3');
+const addContent1 = document.querySelector('.addContent1');
+const addContent4 = document.querySelector('.addContent4');
+addContent1.innerHTML = woodMachine(100);
+addContent4.innerHTML = woodMachine(12200);
 
-codeCSS.classList.add('change');
-codeCSS.classList.add('head3');
+// codeCSS.classList.add('change');
+// codeCSS.classList.add('head3');
 
 // console.log(codeCSS);
